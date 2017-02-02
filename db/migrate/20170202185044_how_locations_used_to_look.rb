7469b0e4c1f71498136b9494e6c427b41f649c94
tasconajoey <joey.tascona@gmail.com>
2017-02-02 14:32:19 -0500

    Fix errors of previous commit

--- db/migrate/20170202185044_how_locations_used_to_look.rb
+++ db/migrate/20170202185044_how_locations_used_to_look.rb
@@ -1,18 +1,12 @@
 class HowLocationsUsedToLook < ActiveRecord::Migration[5.0]
-  def change
-    def up
-      add_column :locations, :locations
-      add_column :locations, :location
-      add_column :locations, :name
-      remove_column :locations, :weather, :string
-      remove_column :locations, :city, :string
-    end
-    def down
-      remove_column :locations, :locations, :integer
-      remove_column :locations, :location, :string
-      remove_column :locations, :name, :string
-      add_column :locations, :weather, :string
-      add_column :locations, :city, :string
-    end
+  def up
+    add_column :locations, :locations, :integer
+    remove_column :locations, :weather
+    remove_column :locations, :city
+  end
+  def down
+    remove_column :locations, :locations
+    add_column :locations, :weather, :string
+    add_column :locations, :city, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170202184022) do
+ActiveRecord::Schema.define(version: 20170202185044) do
 
   create_table "locations", force: :cascade do |t|
-    t.string "name"
-    t.string "location"
-    t.string "weather"
-    t.string "city"
+    t.string  "name"
+    t.string  "location"
+    t.integer "locations"
   end
 
   create_table "parts", force: :cascade do |t|
